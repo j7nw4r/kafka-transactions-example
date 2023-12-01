a8f741bb2e85d5738fe3fd9ec33851a0c0dd81f0
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2023-12-01 09:09:38 -0500

    Adding apikey flow examples

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 ## Purpose
 
-This POC showcases the kafka transactional dataflow. It is meant to be run against a 
+This POC showcases the kafka transactional dataflow.
+
+## Scenerios
+We want to show that a request flow of:
+
+- A transactional producer that commits successfully
+- A transactional producer that aborts
+- A transactional producer that also consumes under a transaction and commits.
+- A transactional producer that also consumes under a transaction and aborts.
+- A consumer that consumes transactionally
+
+
+- Init Transaction, Produce, Consume read_committed, offset commit, commit transaction, Consume read_committed, offset commit
+- Init Transaction, Produce, Consume read_uncommitted, commit transactions.
+- Init Transaction, Produce, Consume read_uncommitted, abort transaction, Consume read_uncommitted
--- kafka-transaction-example/Program.cs
+++ kafka-transaction-example/Program.cs
@@ -6,70 +6,69 @@ internal class kafka_transactions_example
 
     private static async Task Main(string[] args)
     {
-        var producerConfig = new ProducerConfig { BootstrapServers = "127.0.0.1:49695", TransactionalId = "johnathan" };
-        using var p = new ProducerBuilder<Null, string>(producerConfig).Build();
-        p.InitTransactions(TimeSpan.FromSeconds(1));
+        // var producerConfig = new ProducerConfig
+        // {
+        //     BootstrapServers = "127.0.0.1:9092",
+        //     TransactionalId = "johnathan",
+        //     // EnableIdempotence = true
+        // };
+        // using var p = new ProducerBuilder<Null, string>(producerConfig).Build();
+        // p.InitTransactions(TimeSpan.FromSeconds(1));
 
         var commitedConsumerConfig = new ConsumerConfig
         {
-            BootstrapServers = "127.0.0.1:49695",
+            BootstrapServers = "127.0.0.1:9092",
             GroupId = "johnathan-consumer-group",
-            IsolationLevel = IsolationLevel.ReadCommitted,
+            IsolationLevel = IsolationLevel.ReadUncommitted,
             AllowAutoCreateTopics = true,
-            AutoOffsetReset = AutoOffsetReset.Earliest
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = false,
         };
         using var committedConsumer = new ConsumerBuilder<Null, string>(commitedConsumerConfig).Build();
         committedConsumer.Subscribe(CONSUMER_TOPIC_NAME);
-        
-        var uncommittedConsumerConfig = new ConsumerConfig
-        {
-            BootstrapServers = "127.0.0.1:49695",
-            GroupId = "johnathan-consumer-group",
-            IsolationLevel = IsolationLevel.ReadCommitted,
-            AllowAutoCreateTopics = true,
-            AutoOffsetReset = AutoOffsetReset.Earliest
-        };
-        using var uncommittedConsumer = new ConsumerBuilder<Null, string>(uncommittedConsumerConfig).Build();
-        committedConsumer.Subscribe(CONSUMER_TOPIC_NAME);
+        //
+        // var uncommittedConsumerConfig = new ConsumerConfig
+        // {
+        //     BootstrapServers = "127.0.0.1:9092",
+        //     GroupId = "johnathan-consumer-group",
+        //     IsolationLevel = IsolationLevel.ReadCommitted,
+        //     AllowAutoCreateTopics = true,
+        //     AutoOffsetReset = AutoOffsetReset.Earliest,
+        //     EnableAutoCommit = false,
+        // };
+        // using var uncommittedConsumer = new ConsumerBuilder<Null, string>(uncommittedConsumerConfig).Build();
+        // committedConsumer.Subscribe(CONSUMER_TOPIC_NAME);
 
         
         // Produce to consumer topic
-        await PrimeConsumerTopic(p, CONSUMER_TOPIC_NAME);
-
+        // await PrimeConsumerTopic(p, CONSUMER_TOPIC_NAME);
+        
         // Consume from consumer-topic and produce to another topic showcasing the transactional messaging.
-        while (true)
-        {
-            p.BeginTransaction();
+        // p.BeginTransaction();
             
-            // Do some consuming here.
-            var consumeResult = committedConsumer.Consume();
-            Console.WriteLine("consumed " + consumeResult.Message.Value);
-
-            try
-            {
-                var dr = await p.ProduceAsync("test-topic",
-                    new Message<Null, string> { Value = "consumed " + consumeResult.Message.Value });
-                Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
-            }
-            catch (ProduceException<Null, string> e)
-            {
-                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
-            }
-            
-            p.SendOffsetsToTransaction(
-                new List<TopicPartitionOffset>
-                {
-                    consumeResult.TopicPartitionOffset
-                }, committedConsumer.ConsumerGroupMetadata,
-                TimeSpan.FromSeconds(2));
-
-            p.CommitTransaction();
-            
-            consumeResult = committedConsumer.Consume();
-            committedConsumer.Commit();
-            
-            await Task.Delay(1000);
-        }
+        // Do some consuming here.
+        var consumeResult = committedConsumer.Consume();
+        Console.WriteLine("consumed " + consumeResult.Message.Value);
+        
+        // try
+        // {
+        //     var dr = await p.ProduceAsync("test-topic",
+        //         new Message<Null, string> { Value = "consumed "});
+        //     Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+        // }
+        // catch (ProduceException<Null, string> e)
+        // {
+        //     Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+        // }
+        //     
+        // p.SendOffsetsToTransaction(
+        //     new List<TopicPartitionOffset>
+        //     {
+        //         consumeResult.TopicPartitionOffset
+        //     }, committedConsumer.ConsumerGroupMetadata,
+        //     TimeSpan.FromSeconds(2));
+        //
+        // p.AbortTransaction();
     }
 
     private static async Task ConsumeAsync(IConsumer<Null, string> c, string topic)
